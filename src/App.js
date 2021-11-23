d97636a4b37b44ca70a7af91e2c88c27fe30b089
Jameson.Brown <jameson.brown@mx.com>
2021-11-22 20:27:11 -0700

    Added new views and user auth feature

--- src/App.js
+++ src/App.js
@@ -1,17 +1,43 @@
-import React, { Component } from 'react'
-import { Routes, Route } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { Link, Routes, Route } from 'react-router-dom'
 
-class App extends Component {
-  render() {
-    return (
-      <div style={{ textAlign: 'center' }}>
-        <h2>Welcome to Space Race</h2>
-        <Routes>
-          <Route exact path="/" element={<p>See README for details</p>} />
-          <Route element={<p>404: Navigate to Root</p>} />
-        </Routes>
-      </div>
-    )
-  }
+import { auth } from './firebase'
+
+import { Home } from './views/Home'
+import { Login } from './views/Login'
+import { Settings } from './views/Settings'
+import { Level } from './views/Level'
+
+const App = () => {
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    auth.onAuthStateChanged((currentUser) => {
+      currentUser && setUser(currentUser)
+    })
+  })
+
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h2>Welcome to Space Race</h2>
+      <Routes>
+        <Route exact path="/" element={<Home user={user} />} />
+        <Route path="/settings" element={<Settings user={user} />} />
+        <Route path="/login" element={<Login setUser={setUser} user={user} />} />
+        <Route path="/level/:id" element={<Level user={user} />} />
+        {/* If path doesn't match a speecific path, show error with link to Home */}
+        <Route
+          path="*"
+          replace={true}
+          element={
+            <div>
+              <p>404: Path not found</p>
+              <Link to="/">Go to Home</Link>
+            </div>
+          }
+        />
+      </Routes>
+    </div>
+  )
 }
 export default App
